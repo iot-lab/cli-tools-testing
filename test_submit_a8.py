2a36cc21b4278fae12b71ee5e6456d75db6b5b01
Olivier Fambon <olivier.fambon@inria.fr>
2017-01-31 14:29:56 +0100

    isolate get_api()

--- test_submit_a8.py
+++ test_submit_a8.py
@@ -5,14 +5,19 @@ from iotlabcli import experiment
 from iotlabcli.parser.experiment import exp_resources_from_str
 import os
 
-def test_submit_experiment_a8():
-    """ Start experiment"""
+def get_api():
     user = os.environ['IOTLAB_USER']
     passwd = os.environ['IOTLAB_PASS']
+    api = iotlabcli.Api(user, passwd)
+    return api
+
+
+def test_submit_experiment_a8():
+    """ Start experiment"""
     resources = exp_resources_from_str("grenoble,a8,1-2")
     name = "test_exp_A8"
     duration = 2
-    api = iotlabcli.Api(user, passwd)
+    api = get_api()
     exp = experiment.submit_experiment(api, name, duration, [resources])
     exp_id = exp['id']
     experiment.wait_experiment(api, exp_id)
