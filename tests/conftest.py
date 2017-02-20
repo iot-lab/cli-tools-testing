2fcb565927f3bdbcaf5e98ae160473b9dccccab0
Olivier Fambon <olivier.fambon@inria.fr>
2017-02-20 18:48:19 +0100

    tests: add exp_a8 fixture (conftest)

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,3 +17,14 @@ def run(cmd, raw=False):
     except Exception as e:
         pytest.fail(e)
     return ret if raw else json.loads(ret)
+
+
+@pytest.fixture(scope="module")
+def exp_a8():
+    args = "-d 4 -l 2,archi=a8:at86rf231+site=grenoble"
+    ret = run("experiment-cli submit " + args)
+    exp_id = ret["id"]
+    run("experiment-cli wait -i {}".format(exp_id))
+    yield "-i {}".format(exp_id)
+
+    run("experiment-cli stop -i {}".format(exp_id))
