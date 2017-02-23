75f9f48a928a39c129b38d7021e5d9b73ddfbc9b
Olivier Fambon <olivier.fambon@inria.fr>
2017-02-23 11:25:33 +0100

    api_tests: refactor test_submit_a8.py to use fixture exp()
    this makes the test robust to other experiments
    running in the background.
    
    bonus, while at it:
    - add test_get_experiments()
    - add check for 'Error' state after expe stop

--- api_tests/test_submit_a8.py
+++ api_tests/test_submit_a8.py
@@ -17,19 +17,26 @@ def test_submit_experiment_a8_physical(api):
     assert state['state'] == "Running"
 
 
-def test_submit_experiment_a8_logical(api):
+def test_submit_experiment_a8_logical(api, exp):
     name = "test_exp_A8"
     duration = 2
     resources = exp_resources_from_str("2,archi=a8:at86rf231+site=grenoble")
-    exp = experiment.submit_experiment(api, name, duration, [resources])
-    exp_id = exp['id']
+    ret = experiment.submit_experiment(api, name, duration, [resources])
+    exp.id = ret['id']
+    exp_id = exp.id
     experiment.wait_experiment(api, exp_id)
     state = experiment.get_experiment(api, exp_id, 'state')
     assert state['state'] == "Running"
 
 
-def test_stop_experiment(api):
-    # use previous experiment
-    exp_ids = api.get_experiments()  # running experiments
-    exp_id = exp_ids["items"][0]["id"]
-    api.stop_experiment(exp_id)
+def test_get_experiments(api, exp):
+    exps_info = api.get_experiments(state='Running')
+    exps_ids = [ info["id"] for info in exps_info["items"] ]
+    assert exp.id in exps_ids
+
+
+def test_stop_experiment(api, exp):
+    api.stop_experiment(exp.id)
+
+    # stopped experiments eventually report as Error
+    experiment.wait_experiment(api, exp.id, states="Error")
